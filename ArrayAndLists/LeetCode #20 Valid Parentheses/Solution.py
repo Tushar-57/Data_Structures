e11380ae5718eb2a272b34636c827688c9121ff1
Tushar Sharma <tushar.sharma78000@gmail.com>
2025-05-10 18:28:59 -0400

    Adding python solutions as learning.

---
+++ ArrayAndLists/LeetCode #20 Valid Parentheses/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+        brackets = {")": "(", "}": "{", "]": "["}
+        for char in s:
+            if char in "({[":
+                stack.append(char)
+            elif char in ")}]":
+                if not stack or stack.pop() != brackets[char]:
+                    return False
+        return len(stack) == 0
